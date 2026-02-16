1a7c7fd27f58335a86727f3ca04de519edd1bfd5
FazioNico <contact@nicolasfazio.ch>
2026-02-16 10:42:36 +0100

    refactor: improve code formatting and structure in main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,38 @@
-import './style.css'
-import { flashcards} from './flashcards';
+import "./style.css";
+import { flashcards } from "./flashcards";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-
     <h1>Flashcards Quizzz</h1>
+    <div class="flashcards-container"></div>
+    <button class="start-quizzz">Start Quizzz</button>
+  </div>
+`;
 
-    <table>
+const startQuizzz = () => {
+  const elementHtml = document.querySelector(".flashcards-container");
+  elementHtml.innerHTML = `<table>
       <tr>
         <th>Question</th>
         <th>Answer</th>
       </tr>
-      ${flashcards.map(flashcard => `
+      ${flashcards
+        .map(
+          (flashcard) => `
         <tr>
           <td>${flashcard.question}</td>
           <td>${flashcard.answer}</td>
         </tr>
-      `).join('')}
-    </table> 
-  </div>
-`
+      `,
+        )
+        .join("")}
+    </table>`;
+};
 
+document.querySelector(".start-quizzz").addEventListener(
+  "click", 
+  () => {
+    startQuizzz();
+    document.querySelector(".start-quizzz").style.display = "none";
+  }
+);
